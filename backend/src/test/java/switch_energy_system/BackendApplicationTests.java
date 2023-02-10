8b24318ef2d3c6f4562bc863a897f96939864980
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-02-10 11:40:35 +0530

    Initial commit for backend

---
+++ backend/src/test/java/switch_energy_system/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package switch_energy_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
