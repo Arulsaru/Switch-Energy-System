b9688e00e18c78e831ed687fce02efd37cd26a11
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-02-12 01:59:26 +0530

    New Material api imported

--- frontend/src/material/material.module.ts
+++ frontend/src/material/material.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
 
 const materialComponents = [
-    MatIconModule
-]
+  MatIconModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatInputModule,
+  ReactiveFormsModule,
+  MatMenuModule
+];
 
 @NgModule({
   imports: [materialComponents],
