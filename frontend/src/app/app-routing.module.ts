2aee73b3f3b1f3b62767b1c80c09c088e70f710f
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-02-12 01:58:04 +0530

    App routing module updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
+  { path: 'main', component: MainPageComponent },
 ];
 
 @NgModule({
