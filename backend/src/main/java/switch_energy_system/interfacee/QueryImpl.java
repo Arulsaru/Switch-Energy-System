46b1cd1eb508c960798602626ef716faacc9b58a
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-02-12 02:02:34 +0530

    get query for smartmeter id implemented

--- backend/src/main/java/switch_energy_system/interfacee/QueryImpl.java
+++ backend/src/main/java/switch_energy_system/interfacee/QueryImpl.java
@@ -4,7 +4,13 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 public interface QueryImpl {
+
     public default Query getQueryForProviderName(String providerName) {
         return Query.query(Criteria.where("providerName").is(providerName));
     }
+
+    public default Query getQueryForSmartMeterId(String smartMeterId) {
+        return Query.query(Criteria.where("smartMeterId").is(smartMeterId));
+    }
+
 }
