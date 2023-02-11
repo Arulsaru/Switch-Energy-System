5859e626d8d7c05638065066a3662a3e30a885c9
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-02-12 01:58:46 +0530

    App Module.ts updated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,24 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/material/material.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { DescriptionComponent } from './description/description.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ProvidersPageComponent } from './providers-page/providers-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     SignupPageComponent,
-    DescriptionComponent
+    DescriptionComponent,
+    MainPageComponent,
+    ToolbarComponent,
+    ProvidersPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
