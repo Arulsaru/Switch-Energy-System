56abadb09110725f509ecc00db353498c57d002c
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-02-13 14:21:32 +0530

    Interface file name modified

--- frontend/src/app/interface/provider.ts
+++ frontend/src/app/interface/provider.ts
@@ -1,6 +1,6 @@
 export interface providerType {
     providerName: String;
     ratePerWatt: Number;
-    usersList: Array<String>; // list of users using that provider
+    smartMetersList: Array<String>; // list of users using that provider
     enabled: boolean;
 }
