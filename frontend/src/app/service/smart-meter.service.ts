7616fd68b7ff1978a6f1ae0329bc4595d783f9f7
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-02-14 23:04:39 +0530

    smart meter service file modified

--- frontend/src/app/service/smart-meter.service.ts
+++ frontend/src/app/service/smart-meter.service.ts
@@ -3,27 +3,37 @@ import { HttpClient } from '@angular/common/http';
 import { smartMeterType } from '../interface/smart-meter';
 import { Observable } from 'rxjs';
 import { baseURL } from '../constant/constant-variables';
+import { userType } from '../interface/user';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class SmartMeterService {
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    getPendingSmartMeters(): Observable<smartMeterType[]> {
-        return this.http.get<smartMeterType[]>(`${baseURL}smartmeter`);
-    }
+  getPendingSmartMeters(): Observable<smartMeterType[]> {
+    return this.http.get<smartMeterType[]>(`${baseURL}smartmeter`);
+  }
 
-    approveOrRejectSmartMeter(smartMeterId: String, smartMeterStatus: String, providerName: String): Observable<Object> {
-        return this.http.put(`${baseURL}smartmeter/${smartMeterId}/approve-or-reject/${smartMeterStatus}/${providerName}`, null);
-    }
+  approveOrRejectSmartMeter(
+    smartMeterId: String,
+    smartMeterStatus: String,
+    providerName: String
+  ): Observable<Object> {
+    return this.http.put(
+      `${baseURL}smartmeter/${smartMeterId}/approve-or-reject/${smartMeterStatus}/${providerName}`,
+      null
+    );
+  }
 
-    createSmartMeter(providerName: String) {
-        const smartMeter = {
-            userName: 'Arulmozhi',
-            providerName: providerName
-        }
-        return this.http.post(`${baseURL}smartmeter`, smartMeter);
-    }
+  createSmartMeter(providerName: String) {
+    return this.http.post(
+      `${baseURL}smartmeter/Arulmozhi/create/${providerName}`,
+      null
+    );
+  }
+
+  getSmartMeterByUserName(userName: String): Observable<smartMeterType[]> {
+    return this.http.get<smartMeterType[]>(`${baseURL}smartmeter/${userName}`);
+  }
 }
